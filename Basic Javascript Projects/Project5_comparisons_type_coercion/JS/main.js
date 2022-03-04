e4b61310e19f98f59ad67c68dc911d9eb2bb90d9
Christian Watson <81544719+cwatson16@users.noreply.github.com>
2022-03-04 14:31:14 -0800

    Javascript project 5

--- Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -10,14 +10,14 @@ function firstFunction() {
 
 //Function that displays true and false using boolean logic
 function booleanFunction() {
-    document.getElementById("x").innerHTML = 12>2;
-    document.getElementById("y").innerHTML = 200<1;
+    document.getElementById("x").innerHTML = 10>2;
+    document.getElementById("y").innerHTML = 20<1;
 }
 
-//Displays 4 in the console
+//Displays number 4
 console.log(2+2);
 
-//Function utilizing type coercion (combining a string and a number)
+//Function utilizing type coercion (combining a string and a number!)
 function coercion() {
     document.getElementById("z").innerHTML = ("20" + 3);
 }
@@ -31,24 +31,24 @@ function doubleEqualSign() {
     document.getElementById("b").innerHTML = ("b"=="a");
 }
 
-//Function that displays true or false using the === operator
+//Function that displays true or false using === operator
 function tripleEqual() {
-    document.getElementById("c").innerHTML = '3= = =3: '+(3===3);
-    document.getElementById("d").innerHTML = '2= = = "two": '+(2==="two");
-    document.getElementById("e").innerHTML = "1= = = '1': " +(1==='1');
-    document.getElementById("f").innerHTML = '4 = = = 5: ' +(4===5);
+    document.getElementById("c").innerHTML = '4= = =4: '+(4===4);
+    document.getElementById("d").innerHTML = '2= = ="two": '+(2==="two");
+    document.getElementById("e").innerHTML = '1= = ="1": ' +(1==="1");
+    document.getElementById("f").innerHTML = '5 = = =6: ' +(5===6);
 }
 
-//Function that displays true or false using the && and || operator
+//Function that displays true or false using && and || operator
 function andOr() {
-    document.getElementById("q").innerHTML = '"a"= ="a" && 11 < 2 is ' + ("a"=="a" && 11<2);
-    document.getElementById("r").innerHTML = '"a"= ="a" && 2 > 1 is' +("a"=="a" && 2>1);
+    document.getElementById("q").innerHTML = '"a"= ="a" && 10 < 2 is ' + ("a"=="a" && 10<2);
+    document.getElementById("r").innerHTML = '"a"= ="a" && 2 > 1 is ' +("a"=="a" && 2>1);
     document.getElementById("s").innerHTML = '1 = = = 1 | | 2 < 1 is ' +(1===1)||2<1;
-    document.getElementById("t").innerHTML = '"you" = = "u" | | 3 > 90 is ' +("you"=="u" || 3>90);
+    document.getElementById("t").innerHTML = '"you" = = "u" | | 3 > 100 is ' +("you"=="u" || 3>100);
 }
 
-//Function that displays ture or false using the ! operator
-function notOperator() {
-    document.getElementById("not").innerHTML = '! (3 > 1) is ' +!(3>1);
-    document.getElementById("not1").innerHTML = '! (1 > 3) is ' +!(1>3);
+//Function that displays true or false using ! operator
+function notFunction() {
+    document.getElementById("not").innerHTML = '! (20 > 10) is ' +!(20>10);
+    document.getElementById("not2").innerHTML = '! (5 > 10) is ' +!(5>10);
 }
