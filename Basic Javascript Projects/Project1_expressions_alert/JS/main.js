7eae0c0a833142d324bf2497f179eac91074534a
Christian Watson <81544719+cwatson16@users.noreply.github.com>
2022-03-04 00:58:53 -0800

    Javascript exercises

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+window.alert("Error: page not found...\"Where are yewwwwwwwwwwwww\" - Tom Delonge"); //using the window.alert function to display the message in quotes in the browser//
+
+document.write("\"yeah...well...you know...the thing is...uhhhh....yeah.\"");
+
+document.write("\"Sorry losers and haters, but my I.Q. is one of the highest - and you all know it!"  //concatenating a string using the "+" operator//
++ " Please don\'t feel so stupid or insecure,"
++ " It\'s not your fault.\""
++ "-Donald Trump");
+
+var A = "no" + "joke"                                                                //assigning concatenated string values and using the document.write function to display//
+document.write(A); 
+
+document.write(6 + 6);                                                                            //using document.write function to display an expression//
+
+
+let text = "AYYYOOO"
+let result = text.fontcolor("red");                                                 //deprecated syntax, no longer displays colored text in HTML5//
+
+var Family = "The Watsons", Dad = "Christian", Son = "Graham", Brother = "Jonah", Dog = "Chin" //Defining multiple variables and giving values//
+document.write(Dog);                                                                           //using a function to write the value of a variable//
